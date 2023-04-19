4d7168e7c2cd30a4936486988243632af9e32276
Brian <gaylove12960@gmail.com>
2023-04-19 19:13:24 +0700

    f

--- src/navbar/index.jsx
+++
@@ -1,9 +0,0 @@
-function Navbar () {
-    return (
-        <div className="text-3xl">
-            <p>Navbar</p>
-        </div>
-    );
-}
-
-export default Navbar;
---
+++ src/pages/navbar/index.jsx
@@ -0,0 +1,12 @@
+import logo from './././images/logo.png'
+
+function Navbar () {
+    return (
+        <div className="text-3xl m-auto py-3">
+            <img src={logo} alt="" />
+            <p className="text-white">Navbar</p>
+        </div>
+    );
+}
+
+export default Navbar;
