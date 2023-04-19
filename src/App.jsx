0d0eefedb6a8e4817e548cdbbb3190069e226490
Brian <53285097+lucky-brian@users.noreply.github.com>
2023-04-19 14:06:52 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className='m-auto text-center'>
+        <p>My Resume</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/navbar/index.jsx
@@ -0,0 +1,9 @@
+function Navbar () {
+    return (
+        <div className="text-3xl">
+            <p>Navbar</p>
+        </div>
+    );
+}
+
+export default Navbar;
